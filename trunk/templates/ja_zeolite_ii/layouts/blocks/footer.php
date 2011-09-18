d06700f7d8b3d6664b60c63c0a2a95fff6811dd2
nlb.toan@gmail.com <nlb.toan@gmail.com@1850021b-fe53-b100-4ced-e3a1fae0be3c>
2011-09-18 05:53:44 +0000

    deleted all

--- trunk/templates/ja_zeolite_ii/layouts/blocks/footer.php
+++
@@ -1,9 +0,0 @@
-<div id="ja-footer" class="wrap">
-<div class="main">
-	<div class="inner clearfix">
-        <jdoc:include type="modules" name="user3" />
-        <jdoc:include type="modules" name="footer" />
-    </div>
-</div>
-</div>
-
