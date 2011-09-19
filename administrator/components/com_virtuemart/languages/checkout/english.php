af730b7ec841cf1e429a9854f3849e5b79e99051
canh.ntr@gmail.com <canh.ntr@gmail.com@1850021b-fe53-b100-4ced-e3a1fae0be3c>
2011-09-19 16:48:50 +0000

    mutilanguge

--- administrator/components/com_virtuemart/languages/checkout/english.php
+++ administrator/components/com_virtuemart/languages/checkout/english.php
@@ -2,9 +2,10 @@
 if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die( 'Direct Access to '.basename(__FILE__).' is not allowed.' ); 
 /**
 *
+* @version $Id: english.php 1071 2007-12-03 08:42:28Z thepisu $
 * @package VirtueMart
 * @subpackage languages
-* @copyright Copyright (C) 2004-2008 soeren - All rights reserved.
+* @copyright Copyright (C) 2004-2007 soeren - All rights reserved.
 * @translator soeren
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
 * VirtueMart is free software. This version may have been modified pursuant
@@ -50,9 +51,46 @@ $langvars = array (
 	'VM_CHECKOUT_CCV_CODE_TIPTITLE' => 'What\'s the Credit Card Validation Code?',
 	'VM_CHECKOUT_MD5_FAILED' => 'MD5 Check failed',
 	'VM_CHECKOUT_ORDERNOTFOUND' => 'Order not found',
-	'PHPSHOP_EPAY_PAYMENT_CARDTYPE' => 'The payment is
-created by %s <img
-src="/components/com_virtuemart/shop_image/ps_image/epay_images/%s"
-border="0">'
+	'VM_CHECKOUT_PBS_APPROVED_ORDERCOMMENT' => '
+                The Payment Transaction was approved by PBS. \n
+                The Transaction has received the following Transaction Number:\n\n
+                Transaction Number: {transactionnumber}\n',
+	'VM_CHECKOUT_PBS_NOTAPPROVED_ORDERCOMMENT' => '
+                The Payment Transaction was not approved by PBS. \n
+                The Transaction has received the following Transaction Number:\n\n
+                Transaction Number: {transactionnumber}\n',
+	'VM_CHECKOUT_DD_ERROR_0' => 'Merchant/forretningsnummer ugyldigt',
+	'VM_CHECKOUT_DD_ERROR_1' => 'Ugyldigt kreditkortnummer',
+	'VM_CHECKOUT_DD_ERROR_2' => 'Ugyldigt belob',
+	'VM_CHECKOUT_DD_ERROR_3' => 'OrderID mangler eller er ugyldig',
+	'VM_CHECKOUT_DD_ERROR_4' => 'PBS afvisning - (Oftest - ugyldig kortdata, sp?rret kort osv...)',
+	'VM_CHECKOUT_DD_ERROR_5' => 'Intern server fejl hos DanDomain eller PBS',
+	'VM_CHECKOUT_DD_ERROR_6' => 'E-dankort ikke tilladt. Kontakt DanDomain',
+	'VM_CHECKOUT_DD_ERROR_DEFAULT' => 'System fejl',
+	'VM_CHECKOUT_FP_ERROR_1' => 'Error: Transaction declined',
+	'VM_CHECKOUT_FP_ERROR_2' => 'Error: Transaction declined',
+	'VM_CHECKOUT_FP_ERROR_3' => 'Error: wrong number format',
+	'VM_CHECKOUT_FP_ERROR_4' => 'Error: illegal transaction',
+	'VM_CHECKOUT_FP_ERROR_5' => 'Error: no answer',
+	'VM_CHECKOUT_FP_ERROR_6' => 'Error_system_failure',
+	'VM_CHECKOUT_FP_ERROR_7' => 'Error: Card expired',
+	'VM_CHECKOUT_FP_ERROR_8' => 'Error: Communication Failure',
+	'VM_CHECKOUT_FP_ERROR_9' => 'Error: Internal Failure',
+	'VM_CHECKOUT_FP_ERROR_10' => 'Error: Card not registered',
+	'VM_CHECKOUT_FP_ERROR_DEFAULT' => 'Error: unknown Error',
+	'VM_CHECKOUT_WF_ERROR_1' => 'Transaktionen blev ikke godkendt',
+	'VM_CHECKOUT_WF_ERROR_2' => 'Mulig snyd',
+	'VM_CHECKOUT_WF_ERROR_3' => 'Kommunikations fejl',
+	'VM_CHECKOUT_WF_ERROR_4' => 'Kort udlobet',
+	'VM_CHECKOUT_WF_ERROR_5' => 'Intern fejl',
+	'VM_CHECKOUT_WF_ERROR_6' => 'Invalid Transaktion',
+	'VM_CHECKOUT_WF_ERROR_7' => 'System fejl',
+	'VM_CHECKOUT_WF_ERROR_8' => 'Forkert forretningsnummer',
+	'VM_CHECKOUT_WF_ERROR_9' => 'Kortet eksistere ikke',
+	'VM_CHECKOUT_WF_ERROR_10' => 'Kort l?ngen er for kort.',
+	'VM_CHECKOUT_WF_ERROR_11' => 'Transaktion kan ikke gennemfores igennem denne terminal',
+	'VM_CHECKOUT_WF_ERROR_12' => 'Kortejeren har ikke rettigheder til at gennemfore denne transaktion.',
+	'VM_CHECKOUT_WF_ERROR_13' => 'Kortnummeret eksistere ikke',
+	'VM_CHECKOUT_WF_ERROR_14' => 'Error unknown'
 ); $VM_LANG->initModule( 'checkout', $langvars );
 ?>
