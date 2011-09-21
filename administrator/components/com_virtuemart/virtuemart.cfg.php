569eb71497a10da6e6f9dd84e713eb741ef323b8
congbang1988@gmail.com <congbang1988@gmail.com@1850021b-fe53-b100-4ced-e3a1fae0be3c>
2011-09-21 13:50:33 +0000

    Danh muc van phong pham

--- administrator/components/com_virtuemart/virtuemart.cfg.php
+++ administrator/components/com_virtuemart/virtuemart.cfg.php
@@ -1,11 +1,10 @@
-<?php 
+<?php
 if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die( 'Direct Access to '.basename(__FILE__).' is not allowed.' );
 /**
+* The configuration file for VirtueMart
 *
-* @version $Id: virtuemart.cfg-dist.php 1707 2009-03-19 07:52:44Z Aravot $
 * @package VirtueMart
 * @subpackage core
-* @copyright Copyright (C) 2004-2007 - soeren. All rights reserved.
 * @license http://www.gnu.org/copyleft/gpl.html GNU/GPL, see LICENSE.php
 * VirtueMart is free software. This version may have been modified pursuant
 * to the GNU General Public License, and as distributed it includes or
@@ -15,48 +14,52 @@ if( !defined( '_VALID_MOS' ) && !defined( '_JEXEC' ) ) die( 'Direct Access to '.
 *
 * http://virtuemart.net
 */
-	
-global $mosConfig_absolute_path,$mosConfig_live_site, $mosConfig_secret;
+
+global $mosConfig_absolute_path,$mosConfig_live_site;
 if( !class_exists( 'jconfig' )) {
-	$global_lang = $GLOBALS['mosConfig_lang'];
-	
-	@include( dirname( __FILE__ ).'/../../../configuration.php' );
-	
-	$GLOBALS['mosConfig_lang'] = $mosConfig_lang = $global_lang;
+$global_lang = $GLOBALS['mosConfig_lang'];
+
+@include( dirname( __FILE__ ).'/../../../configuration.php' );
+
+$GLOBALS['mosConfig_lang'] = $mosConfig_lang = $global_lang;
 }
 // Check for trailing slash
 if( $mosConfig_live_site[strlen( $mosConfig_live_site)-1] == '/' ) {
-	$app = '';
+$app = '';
 }
 else {
-	$app = '/';
+$app = '/';
 }
-// these path and url definitions here are based on the Joomla configuration
-define( 'URL', $mosConfig_live_site.$app );
-define( 'SECUREURL', $mosConfig_live_site.$app);
+// these path and url definitions here are based on the Joomla! Configuration
+define( 'URL', 'http://localhost/' );
+define( 'SECUREURL', 'http://localhost/' );
 
 if ( (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == '443' ) {
-	define( 'IMAGEURL', SECUREURL .'components/com_virtuemart/shop_image/' );
-	define( 'VM_THEMEURL', SECUREURL.'components/com_virtuemart/themes/default/' );
+define( 'IMAGEURL', SECUREURL .'components/com_virtuemart/shop_image/' );
+define( 'VM_THEMEURL', SECUREURL.'components/com_virtuemart/themes/default/' );
 } else {
-	define( 'IMAGEURL', URL .'components/com_virtuemart/shop_image/' );
-	define( 'VM_THEMEURL', URL.'components/com_virtuemart/themes/default/' );
+define( 'IMAGEURL', URL .'components/com_virtuemart/shop_image/' );
+define( 'VM_THEMEURL', URL.'components/com_virtuemart/themes/default/' );
 }
+define( 'VM_THEMEPATH', $mosConfig_absolute_path.'/components/com_virtuemart/themes/default/' );
+
 define( 'COMPONENTURL', URL .'administrator/components/com_virtuemart/' );
 define( 'ADMINPATH', $mosConfig_absolute_path.'/administrator/components/com_virtuemart/' );
 define( 'CLASSPATH', ADMINPATH.'classes/' );
 define( 'PAGEPATH', ADMINPATH.'html/' );
 define( 'IMAGEPATH', $mosConfig_absolute_path.'/components/com_virtuemart/shop_image/' );
 
-define( 'VM_THEMEPATH', $mosConfig_absolute_path.'/components/com_virtuemart/themes/default/' );
-
-@define('VM_TABLEPREFIX', 'vm' );
-define('VM_PRICE_SHOW_PACKAGING_PRICELABEL', '1' );
-define('VM_PRICE_SHOW_INCLUDINGTAX', '' );
-define('VM_PRICE_ACCESS_LEVEL', 'Public Frontend' );
-define('VM_REGISTRATION_TYPE', 'NORMAL_REGISTRATION' );
+define('PSHOP_IS_OFFLINE', '');
+define('PSHOP_OFFLINE_MESSAGE', 'Our Shop is currently down for maintenance. Please check back again soon.');
+define('USE_AS_CATALOGUE', '');
+define('VM_TABLEPREFIX', 'vm');
+define('VM_PRICE_SHOW_PACKAGING_PRICELABEL', '');
+define('VM_PRICE_SHOW_INCLUDINGTAX', '');
+define('VM_PRICE_ACCESS_LEVEL', 'Public Frontend');
+define('VM_REGISTRATION_TYPE', 'NORMAL_REGISTRATION');
 define('VM_BROWSE_ORDERBY_FIELD', 'product_name');
 define('VM_GENERALLY_PREVENT_HTTPS', '1');
+define('VM_ALLOW_EXTENDED_CLASSES', '');
 define('VM_SHOW_REMEMBER_ME_BOX', '');
 define('VM_REVIEWS_MINIMUM_COMMENT_LENGTH', '100');
 define('VM_REVIEWS_MAXIMUM_COMMENT_LENGTH', '2000');
@@ -69,17 +72,19 @@ define('VM_PROXY_PORT', '');
 define('VM_PROXY_USER', '');
 define('VM_PROXY_PASS', '');
 define('VM_ONCHECKOUT_SHOW_LEGALINFO', '');
-define('VM_ONCHECKOUT_LEGALINFO_SHORTTEXT', '');
+define('VM_ONCHECKOUT_LEGALINFO_SHORTTEXT', 'Returns Policy
+
+You can cancel this order within two weeks after we have received it.
+You can return new, unopened items from a cancelled order within 2 weeks after they have been 
+delivered to you. Items should be returned in their original packaging.
+For more information on cancelling orders and returning items, see the Our Returns Policy page.');
 define('VM_ONCHECKOUT_LEGALINFO_LINK', '');
-define('PSHOP_IS_OFFLINE', '');
-define('PSHOP_OFFLINE_MESSAGE', '<h2>Our Shop is currently down for maintenance.</h2> Please check back again soon.');
-define('USE_AS_CATALOGUE', '');
 define('ENABLE_DOWNLOADS', '');
 define('DOWNLOAD_MAX', '3');
 define('DOWNLOAD_EXPIRE', '432000');
 define('ENABLE_DOWNLOAD_STATUS', 'C');
 define('DISABLE_DOWNLOAD_STATUS', 'X');
-define('DOWNLOADROOT', $mosConfig_absolute_path.'/');
+define('DOWNLOADROOT', 'C:\xampp\htdocs/');
 define('VM_DOWNLOADABLE_PRODUCTS_KEEP_STOCKLEVEL', '');
 define('_SHOW_PRICES', '1');
 define('ORDER_MAIL_HTML', '1');
@@ -88,23 +93,19 @@ define('CATEGORY_TEMPLATE', 'managed');
 define('FLYPAGE', 'flypage.tpl');
 define('PRODUCTS_PER_ROW', '1');
 define('ERRORPAGE', 'shop.error');
-define('DEBUGPAGE', 'shop.debug');
 define('NO_IMAGE', 'noimage.gif');
-define('SEARCH_ROWS', '20');
-define('SEARCH_COLOR_1', '#f9f9f9');
-define('SEARCH_COLOR_2', '#f0f0f0');
 define('DEBUG', '');
 define('SHOWVERSION', '1');
-define('PSHOP_ADD_TO_CART_STYLE', 'add-to-cart_orange.gif');
 define('TAX_VIRTUAL', '1');
 define('TAX_MODE', '1');
 define('MULTIPLE_TAXRATES_ENABLE', '');
 define('PAYMENT_DISCOUNT_BEFORE', '');
-define('PSHOP_ALLOW_REVIEWS', '1');
+define('PAYMENT_DISCOUNT_VAT_ID', '');
+define('PSHOP_ALLOW_REVIEWS', '');
 define('PSHOP_AGREE_TO_TOS_ONORDER', '');
 define('SHOW_CHECKOUT_BAR', '1');
 define('CHECK_STOCK', '');
-define('ENCODE_KEY', md5( $mosConfig_secret ) );
+define('ENCODE_KEY', '647eb761344c6e6f32221dbb275fc33c');
 define('NO_SHIPPING', '');
 define('NO_SHIPTO', '');
 define('AFFILIATE_ENABLE', '');
@@ -142,19 +143,19 @@ define('VM_LOGFILE_FORMAT', '%{timestamp} %{ident} [%{priority}] [%{remoteip}] [
 
 /* OrderByFields */
 global $VM_BROWSE_ORDERBY_FIELDS;
-$VM_BROWSE_ORDERBY_FIELDS = array( 'product_name', 'product_price', 'product_cdate' );
+$VM_BROWSE_ORDERBY_FIELDS = array( 'product_name','product_price','product_cdate' );
 
 /* Shop Modules that run with https only*/
 global $VM_MODULES_FORCE_HTTPS;
 $VM_MODULES_FORCE_HTTPS = array( 'account','checkout' );
 
+// Checkout Steps and their order
 global $VM_CHECKOUT_MODULES;
-$VM_CHECKOUT_MODULES = array('CHECK_OUT_GET_SHIPPING_ADDR' => array('order' => 1,'enabled'=>1),
-		'CHECK_OUT_GET_SHIPPING_METHOD' => array('order' => 2,'enabled'=>1),
-		'CHECK_OUT_GET_PAYMENT_METHOD' => array('order' => 3,'enabled'=>1),
-		'CHECK_OUT_GET_FINAL_CONFIRMATION' => array('order' => 4,'enabled'=>1)
-	);
-							
+$VM_CHECKOUT_MODULES = array( 'CHECK_OUT_GET_SHIPPING_ADDR'=>array('order'=>1,'enabled'=>1),
+'CHECK_OUT_GET_SHIPPING_METHOD'=>array('order'=>2,'enabled'=>1),
+'CHECK_OUT_GET_PAYMENT_METHOD'=>array('order'=>3,'enabled'=>1),
+'CHECK_OUT_GET_FINAL_CONFIRMATION'=>array('order'=>4,'enabled'=>1) );
+
 /* Shipping Methods Definition */
 global $PSHOP_SHIPPING_MODULES;
 $PSHOP_SHIPPING_MODULES[0] = "flex";
