52d526c36583230a7b035cebd28e47c977792a9d
canh.ntr@gmail.com <canh.ntr@gmail.com@1850021b-fe53-b100-4ced-e3a1fae0be3c>
2011-10-03 15:52:51 +0000

    lay-out

--- trunk/templates/ja_zeolite_ii/layouts/blocks/header - Copy.php
+++ trunk/templates/ja_zeolite_ii/layouts/blocks/header - Copy.php
@@ -14,12 +14,27 @@
 </div>
 <!-- //CPANEL -->
 
+
 <div id="ja-header" class="wrap">
 <div class="main">
 <div class="inner clearfix">
 
 
-	<jdoc:include type="modules" name="banner" />
+	<?php
+	$siteName = $this->sitename();
+	if ($this->getParam('logoType')=='image'): ?>
+	<h1 class="logo">
+		<a href="index.php" title="<?php echo $siteName; ?>"><span><?php echo $siteName; ?></span></a>
+	</h1>
+	<?php else:
+	$logoText = (trim($this->getParam('logoType-text-logoText'))=='') ? $config->sitename : $this->getParam('logoType-text-logoText');
+	$sloganText = (trim($this->getParam('logoType-text-sloganText'))=='') ? JText::_('SITE SLOGAN') : $this->getParam('logoType-text-sloganText');?>
+	<div class="logo-text">
+		<h1><a href="index.php" title="<?php echo $siteName; ?>"><span><?php echo $logoText; ?></span></a></h1>
+		<p class="site-slogan"><?php echo $sloganText;?></p>
+	</div>
+	<?php endif; ?>
+	
 	<?php if (!$this->isContentEdit() && $this->countModules('vm-cart')) { ?>
 	<div id="ja-vmcart">
 		<jdoc:include type="modules" name="vm-cart" style="xhtml" />
